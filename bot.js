bd8abebd1350ca8c0da78afe95095c90c47355ec
Breezely <44201082+Breezely@users.noreply.github.com>
2018-11-03 20:09:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Bot By ┃ KillerFox ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(` Bot By KillerFox | $help `,"http://twitch.tv/S-F")
+client.user.setGame(` Bot By Clyde | $help `,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
